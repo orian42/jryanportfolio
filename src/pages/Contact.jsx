6a287d2c1edf16723c7ee0733a40ed66cfe3c24e
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-08-29 15:21:06 -0700

    added form to CONTACT page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,19 @@ export default function Contact() {
 
     return (
         <div>
-            <p>This is the CONTACT content.</p>
+            <h1>CONTACT ME</h1>
+            <form style={{display:'flex', flexDirection:'column', gap:'1rem', width:'50%'}}>
+                <label for="name">Name:</label>
+                <input id="name" type="text" name="name"></input>
+
+                <label for="email">Email:</label>
+                <input id="email" type="text" name="email"></input>
+
+                <label for="message">Message:</label>
+                <textarea id="message" name="message" rows="10" cols="50"></textarea>
+
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
