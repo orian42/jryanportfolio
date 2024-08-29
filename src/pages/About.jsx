037afd409f49c5d2d2fedecf2e85e5e41ec32215
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-08-29 14:06:10 -0700

    added content to ABOUT component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,32 @@
 export default function About() {
 
+
     return (
         <div>
             <h1>ABOUT ME</h1>
-            <img />
+            <div style={{display: 'flex'}}>
+                <img 
+                    src="./src/assets/jason.JPEG"
+                    style={{margin: '5px', 
+                        padding: '5px',
+                        width: '33%'
+                    }}
+                />
+                <div style={{
+                        margin: '5px', 
+                        padding: '5px'}}>
+                <p
+                    style={{
+                        margin: '5px', 
+                        padding: '5px',
+                        fontSize: '24px',
+                        textAlign: 'justify'
+                    }}
+                >
+                    I am a recently retired employee of the California Department of Corrections and Rehabilitation (CDCR) having served for 25 1/2 years. I am seeking to begin the next adventure - this time working as a full-stack web developer. I have always been interested in this field and have even taken several courses to better understand it. I was able to use this knowledge during my time employed with CDCR to streamline repetitive tasks making several processes easier not only on myself but for those with whom I worked around and for. I am excited to be concluding a coding bootcamp next month! I am ready to prove to myself that I am able to succeed in a new career as a developer!
+                </p>
+                </div>
+            </div>
         </div>
     )
 }
