b8d3288aa28e4a652390bbe814c1faab88b0ebbd
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-08-29 15:08:09 -0700

    added content to RESUME section

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,7 +2,32 @@ export default function Resume() {
 
     return (
         <div>
-            <p>This is the RESUME content.</p>
+            <h1>MY RESUME</h1>
+            <p>Download my <a href='./src/assets/resume.pdf' download='resume.pdf'>
+                resume</a></p>
+            <h2>Front-end Proficiencies</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>responsive design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h2>Back-end Proficiencies</h2>
+                <ul>
+                    <li>API's</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL/Sequelize</li>
+                    <li>MondoDB/Mongoose</li>
+                    <li>REST</li>
+                </ul>
+
+            <div>
+
+            </div>
         </div>
     )
 }
