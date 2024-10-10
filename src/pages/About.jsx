9ce4af18b4a92e133e03328e9b8a3f312ef56ee0
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-10-09 18:09:06 -0700

    edited bio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -29,7 +29,7 @@ export default function About() {
                                 overflowY: "auto",
                             }}
                         >
-                            I am a recently retired employee of the California Department of Corrections and Rehabilitation (CDCR) having served for 25 1/2 years. I am seeking to begin the next adventure - this time working as a full-stack web developer. I have always been interested in this field and have even taken several courses to better understand it. I was able to use this knowledge during my time employed with CDCR to streamline repetitive tasks making several processes easier not only on myself but for those with whom I worked around and for. I am excited to be concluding a coding bootcamp this month! I am ready to prove to myself that I am able to succeed in a new career as a developer!
+                            I am a recently retired employee of the California Department of Corrections and Rehabilitation (CDCR) having served for 25 1/2 years. I am seeking to begin the next adventure - this time working as a full-stack web developer. I have always been interested in this field and have even taken several courses to better understand it. I was able to use this knowledge during my time employed with CDCR to streamline repetitive tasks making several processes easier not only on myself but for those with whom I worked around and for. Having completed a coding bootcamp in September 2024, I am ready to prove to myself that I am able to succeed in a new career as a developer!
                         </p>
                     </div>
                 </div>
